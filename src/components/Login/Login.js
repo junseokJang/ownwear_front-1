ec1c19c3b19bef638746cf14155b26176202224e
junseokJang <iphj6403@gmail.com>
2021-09-10 17:00:36 +0900

    0.0.1

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,13 +7,21 @@ export default function Login() {
   return (
     <div className="Login">
       <div id="login-form-container">
+        <h2>ownwear ID 로그인</h2>
           <form onSubmit={loginSubmit} className="login-form">
-              <input type="text" name="email" id="" placeholder="id" />
-              <input type="password" name="pwd" id="" placeholder="pwd" />
-              <input type="submit" value="" />
+              <input type="text" name="email" id="" placeholder="id" /><br/>
+              <input type="password" name="pwd" id="" placeholder="pwd" /><br/>
+              <input type="submit" value="로그인" />
           </form>
       </div>
-      <div id="signup-form-container"></div>
+      <div id="signup-form-container">
+        <h2>외부 ID로 로그인</h2>
+        <ul>
+          <li>facebook</li>
+          <li>naver</li>
+          <li>kakao</li>
+        </ul>
+      </div>
     </div>
   );
 }
